b894d6f3ceb531f84f05e623011d6f19383ac5a8
Manolo Schotsborg <manolo.schotsborg@outlook.com>
2016-02-28 14:25:48 -0300

    add poll selector to main page

--- public/javascripts/pollStorage.js
+++ public/javascripts/pollStorage.js
@@ -1,59 +1,96 @@
-function abc () {
-
 var pollName;
 var categories;
 var votes;
+var allPollsData;
+var index = 0;
+var str = '';
 
-console.log('pollstorage file read');
+function changeChart() {
+    console.log('changeChart called');
+    var selectedPoll = document.getElementById('menuSelector').value;
+    pollName = allPollsData[selectedPoll].pollName;
+    categories = allPollsData[selectedPoll].pollItems.map(categoriesFun);
+    votes = allPollsData[selectedPoll].pollItems.map(votesFun);
+    buildChart();
+}
 
-function categoriesFun (pollItem){
+function buildChart() {
+    var options = {
+        chart: {
+            renderTo: 'container',
+            type: 'bar'
+        },
+        title: {
+            text: pollName
+        },
+        subtitle: {
+            text: 'xxxxxxx'
+        },
+        xAxis: {
+            categories: categories
+        },
+        yAxis: {
+            title: {
+                text: 'Votes'
+            }
+        },
+        series: [{
+            data: votes
+        }]
+    };
+
+    //build chart using options above
+
+    var chart = new Highcharts.Chart(options);
+}
+
+function buildMenuStr(poll) {
+    var pollName = poll.pollName;
+    str = str + '<option value=' + index + '>' + pollName + '</option>'
+    index++;
+}
+
+function buildMenu() {
+    str += '<select id="menuSelector" onchange="changeChart()">';
+    allPollsData.map(buildMenuStr);
+    str += '</select>';
+    $('#menu').append(str);
+}
+
+function categoriesFun(pollItem) {
     return pollItem.categorie;
 }
 
 
-function votesFun (pollItem){
+function votesFun(pollItem) {
     return pollItem.votes;
 }
 
-function buildChart(){
-    var options = {
-              chart: {
-                        renderTo: 'container',
-                        type: 'bar'
-                    },
-                    title: {
-                        text: pollName
-                    },
-                    subtitle: {
-                        text: 'xxxxxxx'  
-                    },
-                    xAxis:  {categories: categories},
-                    yAxis: {
-                        title: {
-                            text: 'Votes'
-                        }
-                    },
-                    series: [{
-                        data: votes
-                    }]
-            };
-    
-//build chart using options above
-
-var chart = new Highcharts.Chart(options);
+function changeChart() {
+    console.log('changeChart called');
+    var selectedPoll = document.getElementById('menuSelector').value;
+    pollName = allPollsData[selectedPoll].pollName;
+    categories = allPollsData[selectedPoll].pollItems.map(categoriesFun);
+    votes = allPollsData[selectedPoll].pollItems.map(votesFun);
+    buildChart();
 }
 
-function success(result){
+
+function success(result) {
     console.log('succes func called');
-    // console.log('result', result);
+    console.log('result', result);
     // console.log('result.polldata', result.pollData[0].pollName);
+    allPollsData = result.pollData;
+    console.log('allPollsData', allPollsData)
     pollName = result.pollData[0].pollName;
     categories = result.pollData[0].pollItems.map(categoriesFun);
     votes = result.pollData[0].pollItems.map(votesFun);
     console.log(pollName, categories, votes)
+    buildMenu();
     buildChart();
 }
-console.log('call getjson');
-$.getJSON('http://localhost:3000/data/polldata', success)
-}
 
+
+function abc() {
+    $.getJSON('http://localhost:3000/data/polldata', success)
+}
